8c63463d6620eb8f10a0fdce471831fac1240a2d
5jeong <wjdgns4385@naver.com>
2025-03-18 20:30:03 +0900

    Persistence Layer 테스트

---
+++ src/main/java/sample/cafekiosk/spring/domain/product/ProductRepository.java
@@ -0,0 +1,14 @@
+package sample.cafekiosk.spring.domain.product;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    /*
+     * SELECT *
+     * FROM prduct
+     * where selling_type in('SELLING','HOLD')
+     */
+    List<Product> findAllBySellingStatusIn(List<ProductSellingStatus> sellingTypes);
+}
