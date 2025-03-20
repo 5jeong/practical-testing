5ee6ed1cc83fbdc0a3f6a7cb616c317df5034e8a
5jeong <wjdgns4385@naver.com>
2025-03-20 15:14:27 +0900

    Presentation Layer 테스트

--- src/main/java/sample/cafekiosk/spring/domain/product/ProductRepository.java
+++ src/main/java/sample/cafekiosk/spring/domain/product/ProductRepository.java
@@ -2,10 +2,11 @@ package sample.cafekiosk.spring.domain.product;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-    /*
+    /***
      * SELECT *
      * FROM prduct
      * where selling_type in('SELLING','HOLD')
@@ -13,4 +14,7 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findAllBySellingStatusIn(List<ProductSellingStatus> sellingTypes);
 
     List<Product> findAllByProductNumberIn(List<String> productNumbers);
+
+    @Query(value = "select p.product_number from product p order by id desc limit 1", nativeQuery = true)
+    String findLatestProductNumber();
 }
